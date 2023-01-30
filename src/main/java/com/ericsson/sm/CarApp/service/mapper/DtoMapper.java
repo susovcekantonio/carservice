91e446ed65a59187a132c039c75a0d9ffeadaab3
susovcekantonio <asusovcek0@gmail.com>
2023-01-30 11:43:21 +0100

    JIRA: us9 Modified getAll() method

--- src/main/java/com/ericsson/sm/CarApp/service/mapper/DtoMapper.java
+++ src/main/java/com/ericsson/sm/CarApp/service/mapper/DtoMapper.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class DtoMapper {
-    public static ClientResponseDto toDto(Client client){
+    public ClientResponseDto toDto(Client client){
         ClientResponseDto clientResponseDto = new ClientResponseDto();
         clientResponseDto.setFirstName(client.getFirstName());
         clientResponseDto.setLastName(client.getLastName());
