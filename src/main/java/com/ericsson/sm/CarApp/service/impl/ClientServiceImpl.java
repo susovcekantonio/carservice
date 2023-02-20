2b9d9676fb6f4d75653fbb6466777f5fda515f46
susovcekantonio <asusovcek0@gmail.com>
2023-02-20 13:13:10 +0100

    JIRA: us23 Refactored mapping using Mapstruct

--- src/main/java/com/ericsson/sm/CarApp/service/impl/ClientServiceImpl.java
+++ src/main/java/com/ericsson/sm/CarApp/service/impl/ClientServiceImpl.java
@@ -2,10 +2,11 @@ package com.ericsson.sm.CarApp.service.impl;
 
 import com.ericsson.sm.CarApp.dto.ClientRequestDto;
 import com.ericsson.sm.CarApp.dto.ClientResponseDto;
+import com.ericsson.sm.CarApp.model.Car;
 import com.ericsson.sm.CarApp.model.Client;
 import com.ericsson.sm.CarApp.repository.ClientRepository;
 import com.ericsson.sm.CarApp.service.ClientService;
-import com.ericsson.sm.CarApp.service.mapper.ClientDtoMapper;
+import com.ericsson.sm.CarApp.service.mapper.ClientMapper;
 import com.ericsson.sm.CarApp.validation.ClientValidation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,30 +15,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ClientServiceImpl implements ClientService {
 
     private final ClientRepository clientRepository;
-    private final ClientDtoMapper clientDtoMapper;
+    private final ClientMapper clientMapper;
     private final ClientValidation clientValidation;
 
     @Override
     public ClientResponseDto save(ClientRequestDto clientRequestDto) {
-        Client client = clientDtoMapper.toDto(clientRequestDto);
+        Client client = clientMapper.toEntity(clientRequestDto);
         clientValidation.validate(client);
 
         Client savedClient = clientRepository.save(client);
+        List<Car> cars = new ArrayList<>();
+        savedClient.setCars(cars);
 
-        ClientResponseDto clientResponseDto = clientDtoMapper.toDto(savedClient);
+        ClientResponseDto clientResponseDto = clientMapper.toDto(savedClient);
 
         return clientResponseDto;
     }
 
     @Override
     public Page<ClientResponseDto> getAll(String firstName, String lastName, Pageable pageable) {
-            return clientRepository.findByFirstOrLastName(firstName,lastName,pageable).map(clientDtoMapper::toDto);
+            return clientRepository.findByFirstOrLastName(firstName,lastName,pageable).map(clientMapper::toDto);
         }
 
     @Override
@@ -45,7 +50,7 @@ public class ClientServiceImpl implements ClientService {
         Client searchedClient = clientRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Client doesn't exist"));
 
         ClientResponseDto clientResponseDto = new ClientResponseDto();
-        clientResponseDto = clientDtoMapper.toDto(searchedClient);
+        clientResponseDto = clientMapper.toDto(searchedClient);
 
         return clientResponseDto;
     }
@@ -64,11 +69,11 @@ public class ClientServiceImpl implements ClientService {
         Client searchedClient = clientRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Client doesn't exist"));
 
 
-        searchedClient = clientDtoMapper.toDto(clientRequestDto);
+        searchedClient = clientMapper.toEntity(clientRequestDto);
 
         clientRepository.save(searchedClient);
 
-        ClientResponseDto clientResponseDto = clientDtoMapper.toDto(searchedClient);
+        ClientResponseDto clientResponseDto = clientMapper.toDto(searchedClient);
 
         return ResponseEntity.ok(clientResponseDto);
     }
